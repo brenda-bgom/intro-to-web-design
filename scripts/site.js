2099b99d00564bfac4d45e20ab7f091ed2cef786
Brenda Ortega <brendabgom@ibm.com>
2026-06-04 16:04:19 -0600

    Added Boostrap

--- scripts/site.js
+++ scripts/site.js
@@ -13,4 +13,16 @@ function ClearForm(){
 
             $('#jqueryBtn').click(function(){
                 $('p').hide();
-            })            
+            })
+            
+$(function(){
+    var pages= ['index', 'about','contact'];
+    var pathname= window.location.pathname;
+
+    $('-nav-link').each(function(item){
+        if(pathname.includes(pages[item])){
+            $(this).addClass('active');
+            $(this).attr('aria-current', 'page');
+        }
+    })
+});
